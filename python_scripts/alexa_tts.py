e022b210dff240963de56124851f5634e1fa7a4e
ark <ark@cho.red>
2021-11-28 08:52:49 -0800

    Added KITCHEN_TV to OFFICE_1 unless conditions.

--- python_scripts/alexa_tts.py
+++ python_scripts/alexa_tts.py
@@ -112,8 +112,8 @@ RULES = (
         "conditions": (OFFICE_1_LIGHT, OFFICE_1_MOTION, OFFICE_1_TV),
         "target": OFFICE_1_ECHO,
         "unless": {
-            "conditions": (DINING_AREA_LIGHT, KITCHEN_LIGHT, LIVING_ROOM_LIGHT,
-                           LIVING_ROOM_TV),
+            "conditions": (DINING_AREA_LIGHT, KITCHEN_LIGHT, KITCHEN_TV,
+                           LIVING_ROOM_LIGHT, LIVING_ROOM_TV),
             "target": LIVING_ROOM_ECHO
         }
     },
