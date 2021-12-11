8a696ed59bebf312c859bfbbc467e67bcc45be5b
ark <ark@cho.red>
2021-12-10 19:09:02 -0800

    Update alexa_tts.py with new group naming standard.

--- python_scripts/alexa_tts.py
+++ python_scripts/alexa_tts.py
@@ -9,51 +9,51 @@ __author__ = "Ark (ark@cho.red)"
 
 BATHROOM_1 = "bathroom_1"
 BATHROOM_1_DOOR = "binary_sensor.bathroom_1_door"
-BATHROOM_1_LIGHT = "group.bathroom_1_bulbs"
+BATHROOM_1_LIGHT = "group.light_bathroom_1"
 BATHROOM_1_ECHO = "media_player.bathroom_1_echo"
 BATHROOM_1_MOTION = "sensor.bathroom_1_last_5m_motion"
 
 BATHROOM_2 = "bathroom_2"
 BATHROOM_2_DOOR = "binary_sensor.bathroom_2_door"
-BATHROOM_2_LIGHT = "group.bathroom_2_bulbs"
+BATHROOM_2_LIGHT = "group.light_bathroom_2"
 BATHROOM_2_ECHO = "media_player.bathroom_2_echo"
 BATHROOM_2_MOTION = "sensor.bathroom_2_last_5m_motion"
 
 BEDROOM_1 = "bedroom_1"
-BEDROOM_1_LIGHT = "group.bedroom_1_bulbs"
+BEDROOM_1_LIGHT = "group.light_bedroom_1"
 BEDROOM_1_ECHO = "media_player.bedroom_1_echo"
 BEDROOM_1_MOTION = "sensor.bedroom_1_last_5m_motion"
 
 CORRIDOR = "corridor"
 CORRIDOR_ECHO = "media_player.corridor_echo"
 
-DINING_AREA_LIGHT = "group.dining_area_bulbs"
+DINING_AREA_LIGHT = "group.light_dining_area"
 
 GARAGE = "garage"
-GARAGE_LIGHT = "group.garage_bulbs"
+GARAGE_LIGHT = "group.light_garage"
 GARAGE_ECHO = "media_player.garage_echo"
 GARAGE_MOTION = "sensor.garage_last_5m_motion"
 
 HALLWAY_MOTION = "group.hallway_motion_sensors"
 
-KITCHEN_LIGHT = "group.kitchen_bulbs"
+KITCHEN_LIGHT = "group.light_kitchen"
 KITCHEN_MOTION = "group.kitchen_motion_sensors"
 KITCHEN_TV = "media_player.kitchen_tv"
 
 LIVING_ROOM = "living_room"
-LIVING_ROOM_LIGHT = "group.living_room_bulbs"
+LIVING_ROOM_LIGHT = "group.light_living_room"
 LIVING_ROOM_ECHO = "media_player.living_room_echo"
 LIVING_ROOM_MOTION = "sensor.living_room_last_5m_motion"
 LIVING_ROOM_TV = "sensor.living_room_tv_plug_power_on"
 
 OFFICE_1 = "office_1"
-OFFICE_1_LIGHT = "group.office_1_bulbs"
+OFFICE_1_LIGHT = "group.light_office_1"
 OFFICE_1_ECHO = "media_player.office_1_echo"
 OFFICE_1_MOTION = "sensor.office_1_last_5m_motion"
 OFFICE_1_TV = "remote.office_1_tv_remote"
 
 OFFICE_2 = "office_2"
-OFFICE_2_LIGHT = "group.office_2_bulbs"
+OFFICE_2_LIGHT = "group.light_office_2"
 OFFICE_2_ECHO = "media_player.office_2_echo"
 OFFICE_2_MOTION = "sensor.office_2_last_5m_motion"
 
